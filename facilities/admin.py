6ae6cdd08c28f05f413a8bd2a3055c41717ed6d1
@Alidat <alvinnahereza@gmail.com>
2025-09-07 12:46:26 +0300

    added facilities

--- facilities/admin.py
+++ facilities/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Facility, Service, Equipment
 
-# Register your models here.
+@admin.register(Facility)
+class FacilityAdmin(admin.ModelAdmin):
+    list_display = ("name", "location", "facility_type", "partner_organization")
+    search_fields = ("name", "location", "partner_organization")
+    list_filter = ("facility_type",)
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("name", "facility", "category", "skill_type")
+    search_fields = ("name", "category", "skill_type")
+    list_filter = ("category", "skill_type")
+
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ("name", "facility", "inventory_code", "usage_domain", "support_phase")
+    search_fields = ("name", "inventory_code", "usage_domain")
+    list_filter = ("usage_domain", "support_phase")
