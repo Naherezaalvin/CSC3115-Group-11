a8cba2c155a963416a9b247bcf44be9ae51f713b
@Alidat <alvinnahereza@gmail.com>
2025-09-07 11:55:48 +0300

    feat: added installed apps to settings

--- collaborate/settings.py
+++ collaborate/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'facilities',
+    'participants',
+    'programs',
+    'projects',
 ]
 
 MIDDLEWARE = [
