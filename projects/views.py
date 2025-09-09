95de3bb2178c60f9265882704e0ad8e69c4be425
unknown <madrinenakirijja9@gmail.com>
2025-09-09 10:18:20 +0300

    Added views.py code

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Project, Outcome
 
-# Create your views here.
+class ProjectListView(ListView):
+    model = Project
+    template_name = "projects/project_list.html"
+    context_object_name = "projects"
+
+class ProjectDetailView(DetailView):
+    model = Project
+    template_name = "projects/project_detail.html"
+    context_object_name = "project"
+
+class OutcomeListView(ListView):
+    model = Outcome
+    template_name = "projects/outcome_list.html"
+    context_object_name = "outcomes"
+
+class OutcomeDetailView(DetailView):
+    model = Outcome
+    template_name = "projects/outcome_detail.html"
+    context_object_name = "outcome"
+
+
