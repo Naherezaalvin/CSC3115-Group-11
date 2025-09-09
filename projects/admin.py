10383eab0e81a9719e8a3f25260998a02f415f44
unknown <madrinenakirijja9@gmail.com>
2025-09-09 10:16:09 +0300

    Added admin.py code

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Project, Outcome
 
-# Register your models here.
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "program", "facility", "nature_of_project", "prototype_stage")
+    search_fields = ("title", "innovation_focus", "description")
+    list_filter = ("nature_of_project", "prototype_stage", "program", "facility")
+
+@admin.register(Outcome)
+class OutcomeAdmin(admin.ModelAdmin):
+    list_display = ("title", "project", "outcome_type", "commercialization_status")
+    search_fields = ("title", "description", "outcome_type")
+    list_filter = ("outcome_type", "commercialization_status")
