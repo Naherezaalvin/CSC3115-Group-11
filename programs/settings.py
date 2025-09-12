cc5484ad3c1e6c0ac10ea68f43f707fc159d3101
element44 <roslerruthenium204@gmail.com>
2025-09-12 15:03:54 +0300

    settings

---
+++ programs/settings.py
@@ -0,0 +1,15 @@
+# config/settings.py (add or confirm)
+INSTALLED_APPS = [
+    # ...
+    "rest_framework",
+    "apps.capstone",
+]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticatedOrReadOnly"],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
+}
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
