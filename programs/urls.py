aeab951c866ec593ac6ee5d94a2ad6a4e32e72db
element44 <roslerruthenium204@gmail.com>
2025-09-12 15:02:09 +0300

    url editted

--- programs/urls.py
+++ programs/urls.py
@@ -10,3 +10,17 @@ router.register(r"outcomes", OutcomeViewSet, basename="outcome")
 urlpatterns = [
     path("api/", include(router.urls)),
 ]
+
+# config/urls.py
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("apps.capstone.urls")),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
